a05592970f859692f2c153d63eea2ab05ede5af4
Hugo Vallada <valladahugo@gmail.com>
2021-02-02 13:08:51 -0300

    Commit inicial do projeto

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import path from "path";
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(path.dirname("."), "views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.listen(3000, (err) => {
+  if (err) {
+    console.log("Não foi possível iniciar");
+  } else {
+    console.log("ConvertMyMoney está online");
+  }
+});
