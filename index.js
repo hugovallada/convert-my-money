56837b694048ff410984529eb3ee8beb8fe495a6
Hugo Vallada <valladahugo@gmail.com>
2021-02-03 08:46:40 -0300

    Adiciona página de conversão

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 import express from "express";
 import path from "path";
+import { conversao, toMoney } from "./lib/convert.js";
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(path.dirname("."), "views"));
 
-app.use(express.static(path.join(path.dirname("."), 'public')))
+app.use(express.static(path.join(path.dirname("."), "public")));
 
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.get("/cotacao", (req, res) => {
+  const { cotacao, quantidade } = req.query;
+
+  if (cotacao > 0 && quantidade > 0) {
+    const valorConvertido = toMoney(conversao(cotacao, quantidade));
+    res.render("cotacao", {
+      valorConvertido: valorConvertido,
+      quantidade: toMoney(quantidade),
+      cotacao: toMoney(cotacao),
+      error: false,
+    });
+  } else {
+    res.render("cotacao", {
+      error: "Valores inválidos",
+    });
+  }
 });
 
 app.listen(3000, (err) => {
